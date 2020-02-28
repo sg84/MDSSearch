b6bacf1634843b5b9b04fde61ff6d51df2a74d95
Stuart Green <stuart@root9.net>
2020-02-28 11:54:08 +0000

    Made the cycle.sh command generic so should work in all situations

--- cycle.sh
+++ cycle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop $(docker ps -q)
 docker build . -t smex_extension
-docker run -d -p 443:443 -v /home/sysadmin/smex_api_search/web:/var/www/localhost/htdocs -t smex_extension:latest
+docker run -d -p 443:443 -v $(pwd)/web:/var/www/localhost/htdocs -t smex_extension:latest
